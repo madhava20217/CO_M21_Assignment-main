d847eae6270395b4ff43c49db8b658066dcf7185
MohitJain617 <mohit20221@iiitd.ac.in>
2021-08-17 18:19:36 +0530

    executionengines

---
+++ SimpleSimulator/ExecutionEngine.py
@@ -0,0 +1,51 @@
+class ExecutionEngine:
+	def __init__(self, memory, register):
+		self.memory = memory
+		self.register = register
+	
+	def execute(self,instr,cycle=0):
+		opcode = instr[0:5]
+		if(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		elif(opcode == "00000"):
+			#
+		
+		return [halted,pc]
+	
+	
