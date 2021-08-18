fd1da3dee4a609b764a4b04c29fef914b32467be
MohitJain617 <mohit20221@iiitd.ac.in>
2021-08-18 13:11:55 +0530

    pc as parameter and remaining type e funcitons coded

--- SimpleSimulator/ExecutionEngine.py
+++ SimpleSimulator/ExecutionEngine.py
@@ -3,10 +3,12 @@ class ExecutionEngine:
 		self.memory = memory
 		self.register = register
 	
-	def execute(self,instr,cycle=0):
+	def execute(self,instr,pc,cycle=0):
 		opcode = instr[0:5]
 		halted = False
-		pc = 
+		#pc increment by 1
+		pc = "{:08}".format(int(bin(int(pc,2)+1)[2:]))
+
 		if(opcode == "00000"):
 			#
 		elif(opcode == "00001"):
@@ -41,11 +43,14 @@ class ExecutionEngine:
 			if(self.jmp(instr)== True):
 				pc = instr[8:16]
 		elif(opcode == "10000"):
-			self.jlt(instr)
+			if(self.jlt(instr)== True):
+				pc = instr[8:16]
 		elif(opcode == "10001"):
-			self.jgt(instr)
+			if(self.jgt(instr)== True):
+				pc = instr[8:16]
 		elif(opcode == "10010"):
-			self.je(instr)
+			if(self.je(instr)== True):
+				pc = instr[8:16]
 		elif(opcode == "10011"):
 			#
 		
@@ -67,4 +72,20 @@ class ExecutionEngine:
 	
 	def jmp(self, instr):
 		return True 
+		
+	def jlt(self, instr):
+		flags = self.register.getVal(7) 
+		#vlge
+		if(flags[-3] == '1'): return True
+		else: return False
+	def jgt(self, instr):
+		flags = self.register.getVal(7) 
+		#vlge
+		if(flags[-2] == '1'): return True
+		else: return False
+	def je(self, instr):
+		flags = self.register.getVal(7) 
+		#vlge
+		if(flags[-1] == '1'): return True
+		else: return False
 		
--- SimpleSimulator/main.py
+++ SimpleSimulator/main.py
@@ -7,13 +7,13 @@ def main():
 	memory = Memory()
 	registerFile = RegisterFile()
 	executionEngine = ExecutionEngine(memory,registerFile)
-	PC = ProgramCounter(0)
+	PC = ProgramCounter("0"*8)
 	halted = False 
 	cycle = 0
 
 	while not halted:
 		inst = memory.fetch(PC.getVal(),cycle)
-		halted, nextPC = executionEngine.execute(inst,cycle)
+		halted, nextPC = executionEngine.execute(inst,PC.getVal(),cycle)
 		PC.dump()
 		registerFile.dump()
 		PC.update(nextPC)
