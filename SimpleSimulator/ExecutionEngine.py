19754cd7606427b473458189afb64da204aa0d74
Abhimanyu Bhatnagar <abhimanyubhatnagar@outlook.com>
2021-08-18 15:37:04 +0530

    Completed my functions. Need to take care of resetting flag after every instruction

--- SimpleSimulator/ExecutionEngine.py
+++ SimpleSimulator/ExecutionEngine.py
@@ -14,9 +14,9 @@ class ExecutionEngine:
 		elif(opcode == "00001"):
 			#
 		elif(opcode == "00010"):
-			#
+			self.mov_imm(instr)
 		elif(opcode == "00011"):
-			#
+			self.mov_reg(instr)
 		elif(opcode == "00100"):
 			self.ld(instr)
 		elif(opcode == "00101"):
@@ -24,11 +24,11 @@ class ExecutionEngine:
 		elif(opcode == "00110"):
 			#
 		elif(opcode == "00111"):
-			#
+			self.divide(instr)
 		elif(opcode == "01000"):
-			#
+			self.rs(instr)
 		elif(opcode == "01001"):
-			#
+			self.ls(instr)
 		elif(opcode == "01010"):
 			#
 		elif(opcode == "01011"):
@@ -36,9 +36,9 @@ class ExecutionEngine:
 		elif(opcode == "01100"):
 			#
 		elif(opcode == "01101"):
-			#
+			self.invert(instr)
 		elif(opcode == "01110"):
-			#
+			self.cmp(instr)
 		elif(opcode == "01111"):
 			if(self.jmp(instr)== True):
 				pc = instr[8:16]
@@ -55,7 +55,6 @@ class ExecutionEngine:
 			#
 		
 		return [halted,pc]
-	
 	def ld(self, instr):
 		#binary string to integer
 		reg_addr = int(instr[5:8],2)
@@ -69,7 +68,6 @@ class ExecutionEngine:
 		mem_addr = int(instr[8:16],2)
 		#setting memory value
 		self.memory.setData(mem_addr, self.register.getVal(reg_addr))
-	
 	def jmp(self, instr):
 		return True 
 		
@@ -88,4 +86,52 @@ class ExecutionEngine:
 		#vlge
 		if(flags[-1] == '1'): return True
 		else: return False
+	def mov_imm(instr):
+		register1 = int(instr[5:8],2)
+		imm = int(instr[8:],2)
+		self.register.setVal(register1,"{:016d}".format(int(bin(imm)[2:])))
+	def rs(instr):
+		register1 = int(instr[5:8],2)
+		imm = int(instr[8:],2)
+		result = int(self.register.getVal(register1)) >> imm
+		self.register.setVal(register1,"{:016d}".format(int(bin(result)[2:])))
+	def ls(instr):
+		register1 = int(instr[5:8],2)
+		imm = int(instr[8:],2)
+		result = int(self.register.getVal(register1)) << imm
+		self.register.setVal(register1,"{:016d}".format(int(bin(result)[2:])))
+	def mov_reg(instr):
+		register1 = int(instr[5:8],2)
+		register2 = int(instr[5:8],2)
+		self.register.setVal(register1,self.register.getVal(register2))
+	def divide(instr):
+		register1 = int(instr[5:8],2)
+		register2 = int(instr[5:8],2)
+		quotient = int(self.register.getVal(register1)) // int(self.register.getVal(register2))
+		self.register.setVal(register1,"{:016d}".format(int(bin(quotient)[2:])))
+	def invert(instr):
+		register1 = int(instr[5:8],2)
+		register2 = int(instr[5:8],2)
+		output = self.register.getVal(register2)
+		for i in range(0,16):
+			if(output[i] == "0"):
+				output[i] = "1"
+			elif (output[i] == "1"):
+				output[i] = "0"
+		self.register.setVal(register1,output)
+	def cmp(instr):
+		register1 = int(instr[5:8],2)
+		register2 = int(instr[5:8],2)
+		val1 = int(self.register.getVal(register1))
+		val2 = int(self.register.getVal(register2))
+		flag
+		if val1 > val2:
+			flag = "{:016d}".format(int(bin(2)[2:]))
+		elif val1 < val2:
+			flag = "{:016d}".format(int(bin(4)[2:]))
+		elif val1 == val2:
+			flag = "{:016d}".format(int(bin(8)[2:]))
+		self.register.setVal(7,flag)
+
+
 		
