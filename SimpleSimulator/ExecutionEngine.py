126a25191a450d3690b426a23a83b8b48290d2fb
madhava20217 <88394905+madhava20217@users.noreply.github.com>
2021-08-19 18:56:10 +0530

    type A instructions and hlt

--- SimpleSimulator/ExecutionEngine.py
+++ SimpleSimulator/ExecutionEngine.py
@@ -10,9 +10,11 @@ class ExecutionEngine:
 		pc = "{:08}".format(int(bin(int(pc,2)+1)[2:]))
 
 		if(opcode == "00000"):
-			#
+			#for add instruction
+			self.add(instr)
 		elif(opcode == "00001"):
-			#
+			#for sub instruction
+			self.sub(instr)
 		elif(opcode == "00010"):
 			self.mov_imm(instr)
 		elif(opcode == "00011"):
@@ -22,7 +24,8 @@ class ExecutionEngine:
 		elif(opcode == "00101"):
 			self.st(instr)
 		elif(opcode == "00110"):
-			#
+			#for mul instruction
+			self.mul(instr)
 		elif(opcode == "00111"):
 			self.divide(instr)
 		elif(opcode == "01000"):
@@ -30,11 +33,14 @@ class ExecutionEngine:
 		elif(opcode == "01001"):
 			self.ls(instr)
 		elif(opcode == "01010"):
-			#
+			#for xor operation
+			self.logicalxor(instr)
 		elif(opcode == "01011"):
-			#
+			#for OR instruction
+			self.logicalor(instr)
 		elif(opcode == "01100"):
-			#
+			#for AND instruction
+			self.logicaland(instr)
 		elif(opcode == "01101"):
 			self.invert(instr)
 		elif(opcode == "01110"):
@@ -52,7 +58,8 @@ class ExecutionEngine:
 			if(self.je(instr)== True):
 				pc = instr[8:16]
 		elif(opcode == "10011"):
-			#
+			self.register.setVal(7, '0'*16)					#resetting FLAGS
+			halted = True
 		
 		return [halted,pc]
 	def ld(self, instr):
@@ -135,3 +142,109 @@ class ExecutionEngine:
 
 
 		
+	def add(self, instr):
+		'''Input: string operand
+			Output: adds two registers and checks for overflow
+		function for adding operands and storing result
+		have to check for overflow (+ve only)'''
+		outputreg = int(instr[5:8], 2)
+		regop1 = int(instr[8:11], 2)
+		regop2 = int(instr[11:14], 2)
+		regval1 = self.register.getVal(regop1)		#searching RF for value for 1
+		regval2 = self.register.getVal(regop2)		#searching RF for value for 2
+		sum = regval1 + regval2						#sum
+		sum = "{:016d}".format(int(bin(sum)[2:]))
+		if len(sum)>16:
+			self.register.setVal(7, '0'*12+'1'+'0'*3)
+			#overflow set in register file
+		else:
+			self.register.setVal(7, '0'*16)			#in case no overflow, resetting FLAGS
+		sum = sum[-1:-17:-1]	#inverting and taking last 16 bits
+		sum = sum[-1::-1]	#inverting back
+		self.register.setVal(outputreg, sum)
+
+	def sub(self, instr):
+		'''Input: string operand
+		Output: subtracts two registers and checks for underflow
+		If underflowed, sets answer to 0 and sets overflow bit'''
+		outputreg = int(instr[5:8], 2)
+		regop1 = int(instr[8:11], 2)
+		regop2 = int(instr[11:14], 2)
+		regval1 = self.register.getVal(regop1)
+		regval2 = self.register.getVal(regop2)
+		difference = regval1 - regval2		#subtraction
+		if(difference<0):
+			difference = 0
+			self.register.setVal(7, '0'*12+'1'+'0'*3)	#setting FLAGS register overflow bit
+		else:
+			self.register.setVal(7, '0'*16)				#in case no underflow, resetting FLAGS
+		
+		#no OVERFLOW can occur since operands are whole number containing registers
+		#only need to check for underflow
+		
+		difference = "{:016d}".format(int(bin(difference)[2:]))	#converting to bin
+		self.register.setVal(outputreg, difference)
+
+
+	def mul(self, instr):
+		'''Input: string operand
+		Output: multiplies the two numbers in the registers,
+		sets the overflow bit if needed'''
+		outputreg = int(instr[5:8], 2)
+		regop1 = int(instr[8:11], 2)
+		regop2 = int(instr[11:14], 2)
+		regval1 = self.register.getVal(regop1)
+		regval2 = self.register.getVal(regop2)
+		product = regval1 * regval2
+		product = "{:016d}".format(int(bin(product)[2:]))
+		if len(product) > 16:
+			self.register.setVal(7, '0'*12+'1'+'0'*3)		#sets flag register
+		else:
+			self.register.setVal(7, '0'*16)					#in case no overflow, resetting FLAGS
+		product = product[-1:-17:-1]	#inverting and taking last 16 bits
+		product = product[-1::-1]	#inverting back
+		self.register.setVal(outputreg, product)
+
+	def logicalxor(self, instr):
+		'''Input: string operand
+		output: takes xor of the two numbers in the registers, sets overflow bits if needed'''
+		outputreg = int(instr[5:8], 2)
+		regop1 = int(instr[8:11], 2)
+		regop2 = int(instr[11:14], 2)
+		regval1 = self.register.getVal(regop1)
+		regval2 = self.register.getVal(regop2)
+		xorval = regval1^regval2
+		xorval = "{:016d}".format(int(bin(xorval)[2:]))
+		if(len(xorval>16)):self.register.setVal(7, '0'*12+'1'+'0'*3)
+		xorval = xorval[-1:-17:-1]	#taking first 16 bits
+		xorval = xorval[-1::-1]	
+		self.register.setVal(outputreg, xorval)
+		self.register.setVal(7, '0'*16)	#resetting FLAGS
+
+	def logicalOr(self, instr):
+		'''Input: takes string operand
+		Output: writes back to register file, sets overflow bit if required (doesn't)'''
+		outputreg = int(instr[5:8], 2)
+		regop1 = int(instr[8:11], 2)
+		regop2 = int(instr[11:14], 2)
+		regval1 = self.register.getVal(regop1)
+		regval2 = self.register.getVal(regop2)
+		orval = regval1|regval2
+		orval = "{:016d}".format(int(bin(orval)[2:]))
+		#since OR can't set overflow bits, no overflow checks implemented
+		self.register.setVal(outputreg, orval)
+		self.register.setVal(7, '0'*16)	#resetting FLAGS
+
+	def logicaland(self, instr):
+		'''Input: takes string operand
+		Output: writes back to register file after computing AND of the two registers
+		Does not set overflow bits, since it can't overflow with bitwise operations'''
+		outputreg = int(instr[5:8], 2)
+		regop1 = int(instr[8:11], 2)
+		regop2 = int(instr[11:14], 2)
+		regval1 = self.register.getVal(regop1)
+		regval2 = self.register.getVal(regop2)
+		andval = regval1&regval2
+		andval = "{:016d}".format(int(bin(andval)[2:]))
+		self.register.setVal(outputreg, andval)
+		self.register.setVal(7, '0'*16)	#resetting FLAGS
